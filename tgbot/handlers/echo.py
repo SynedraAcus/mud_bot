cd4fc8a5874e4bb58c4d049e330813c0edbc2d3b
synedraacus <alexeymorozov1991@gmail.com>
2023-07-02 23:29:35 +0200

    Some fixes and Echo endpoints prints errors

--- tgbot/handlers/echo.py
+++ tgbot/handlers/echo.py
@@ -4,21 +4,32 @@ from tgbot.clients.redis_client import RedisClient
 
 
 async def custom_echo(message: types.Message):
-    text = f"Here's what you said: {message.text}"
+    stripped = message.text[6:]  # filter only sends msgs starting with '/echo '
+    text = f"Here's what you said: {stripped}"
     await message.answer(text)
 
 
 async def postrges_placeholder(message: types.Message):
     client = PostgresClient()  # Assumes that this object is a singleton
-    await message.answer(client.get_random_placeholder())
+    try:
+        text = client.get_random_placeholder()
+    except Exception as e:
+        text = f"Something wrong with postgreSQL:\n{e}"
+    await message.answer(text)
 
 
 async def redis_placeholder(message: types.Message):
     client = RedisClient()
-    await message.answer(client.get_placeholder())
+    try:
+        text = client.get_placeholder()
+        if not text:
+            text = "No `test_line` in Redis DB"
+    except Exception as e:
+        text = f"Something wrong with Redis:\n{e}"
+    await message.answer(text)
 
 
 def register_echo(dp: Dispatcher):
     dp.register_message_handler(custom_echo, commands="echo")
-    dp.register_message_handler(postrges_placeholder, commands="placeholder")
+    dp.register_message_handler(postrges_placeholder, commands="postgres")
     dp.register_message_handler(redis_placeholder, commands="redis")
